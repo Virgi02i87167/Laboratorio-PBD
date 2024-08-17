39db0cac87f03cbca9dd3974569e0990b2fd1e44
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2024-08-16 23:54:15 -0600

    cambios

--- ejercicio4.sql
+++ ejercicio4.sql
@@ -1,3 +1,35 @@
---Hacer una consulta que procese las ordenes de compra una por una y eval�e si una orden "Pendiente" tiene m�s de 30 d�as si cumple con la condici�n cambiar el status de la orden de Pendiente 
---a Expirada sino dejarla como Pendiente. Use while.
+--Hacer una consulta que procese las ordenes de compra una por una y evalúe si una orden "Pendiente" 
+--tiene más de 30 días si cumple con la condición cambiar el status de la orden de Pendiente a Expirada 
+--sino dejarla como Pendiente. Use while.
+
+
+DECLARE @OrderID INT;
+DECLARE @OrderDate DATE;
+DECLARE @Status NVARCHAR(50);
+
+DECLARE OrderCursor CURSOR FOR
+SELECT [OrderID], [OrderDate], [Status]
+FROM [Bookstore].[dbo].[Orders]
+WHERE [Status] = 'Pending';
+
+OPEN OrderCursor;
+
+FETCH NEXT FROM OrderCursor INTO @OrderID, @OrderDate, @Status;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    IF DATEDIFF(DAY, @OrderDate, GETDATE()) > 30
+    BEGIN
+        
+        UPDATE [Bookstore].[dbo].[Orders]
+        SET [Status] = 'Expirada'
+        WHERE [OrderID] = @OrderID;
+    END
+
+    FETCH NEXT FROM OrderCursor INTO @OrderID, @OrderDate, @Status;
+END
+
+CLOSE OrderCursor;
+DEALLOCATE OrderCursor;
+
 
